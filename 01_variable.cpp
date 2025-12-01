dce980636e9526caa096ed7b61a4e94b365d0294
zwx <3255268575@qq.com>
2025-12-01 09:47:59 +0000

    climits

--- 01_variable.cpp
+++ 01_variable.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <climits>
+#include <limits>
 using namespace std;
 int main()
 {
@@ -27,6 +28,10 @@ int main()
 	// test sizeof
 	cout << "size of short is " << sizeof(short) << endl;
 	cout << "size of int is " << sizeof(int) << endl;
+	cout << "size of float is " << sizeof(float) << endl;
+	cout << "size of double is " << sizeof(double) << endl;
+	cout << "size of long is " << sizeof(long) << endl;
+	cout << "size of long long is " << sizeof(long long) << endl;
 
 	return 0;
 }
