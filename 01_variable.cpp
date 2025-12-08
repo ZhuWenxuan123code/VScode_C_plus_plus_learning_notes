fd827419573aedabc3dc848055d8cc59687d3c6c
Zhuwen Xuan <3255268575@qq.com>
2025-12-08 20:45:29 +0800

    add boundary value tests for short type

--- 01_variable.cpp
+++ 01_variable.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <climits>
 #include <limits>
+#define ZERO 0
 using namespace std;
 int main()
 {
@@ -33,5 +34,28 @@ int main()
 	cout << "size of long is " << sizeof(long) << endl;
 	cout << "size of long long is " << sizeof(long long) << endl;
 
+	// test the boundary value of short
+	cout << "=====testing the boundary value of short type=====" << endl;
+	short sam = SHRT_MAX;										 // initialize a variable to max value
+	unsigned short sue = sam;									 // okay if variable sam already defined
+	cout << "Sam has " << sam << " dollars and Sue has " << sue; // 32767 32768
+	cout << " dollars deposited." << endl
+		 << "Add $1 to each account." << endl
+		 << "Now ";
+	sam = sam + 1;
+	sue = sue + 1;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited.\nPoor Sam!" << endl;
+	sam = ZERO;
+	sue = ZERO;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited." << endl;
+	cout << "Take $1 from each account." << endl
+		 << "Now ";
+	sam = sam - 1;
+	sue = sue - 1;
+	cout << "Sam has " << sam << " dollars and Sue has " << sue;
+	cout << " dollars deposited." << endl
+		 << "Lucky Sue!" << endl;
 	return 0;
 }
