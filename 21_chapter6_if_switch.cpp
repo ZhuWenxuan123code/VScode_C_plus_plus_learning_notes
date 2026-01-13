e65c4f9bc1983c0d747a75bb5f2c9b52acfad1c4
zhuwenxuan <3255268575@qq.com>
2026-01-13 20:14:29 +0800

    add basic if-else structure to demonstrate conditional statements

---
+++ 21_chapter6_if_switch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    a = 10;
+    if (a)
+    {
+        cout << "a is non-zero" << endl;
+    }
+    else
+    {
+        cout << "a is zero" << endl;
+    }
+}
