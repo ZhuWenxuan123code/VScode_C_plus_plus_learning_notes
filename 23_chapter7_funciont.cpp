7b011610c1827cd1a97fea15ae5b11a39188fe40
zhuwenxuan <3255268575@qq.com>
2026-01-14 20:47:14 +0800

    add example demonstrating sizeof operator with arrays and pointers

---
+++ 23_chapter7_funciont.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+
+    int arr[4] = {1, 2, 3, 5};
+    int *p = arr;
+    sizeof arr;
+    sizeof p;
+    std::cout << "sizeof arr: " << sizeof arr << std::endl;
+    std::cout << "sizeof p: " << sizeof p << std::endl; // 由操作系统决定，
+    return 0;
+}
