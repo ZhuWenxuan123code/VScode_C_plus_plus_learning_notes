4dff7ee3ebc8edc38a75fda9b2496e82c2ae9a35
zhuwenxuan <3255268575@qq.com>
2026-01-26 21:56:37 +0800

    add implementation of Swap function template and main program for demonstration

---
+++ 26_chapter8_function_template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+// 再添加关键字typename之前，C++使用关键字 class 来创建模板如下：
+// 关键字typename和 class 在模板函数中是等价的，尽量使用typename
+template <typename T>
+void Swap(T &a, T &b)
+{
+    T temp;
+    temp = b;
+    b = a;
+    a = temp;
+}
+int main()
+{
+    int num1 = 1;
+    int num2 = 2;
+    cout << "num1:" << num1 << endl;
+    cout << "num2:" << num2 << endl;
+    Swap(num1, num2);
+    cout << "num1:" << num1 << endl;
+    cout << "num2:" << num2 << endl;
+    return 0;
+}
