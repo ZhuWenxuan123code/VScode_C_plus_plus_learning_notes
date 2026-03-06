a1437f5363fb172e3afc854f16d508359b3f0e89
Zhuwen Xuan <3255268575@qq.com>
2026-03-07 00:10:31 +0800

    add exception handling for division by zero in average calculation

--- 29_exception_handling.cpp
+++ 29_exception_handling.cpp
@@ -3,5 +3,15 @@ using namespace std;
 int main()
 {
     cout << "Hello World!" << endl;
+    cout << "Enter,x:";
+    int x;
+    cin >> x;
+    cout << "Enter,y:";
+    int y;
+    cin >> y;
+    // 此处需要进行异常处理，防止除数为0的情况
+    int average = 2.0 * x * y / (x + y);
+    cout << "Average: " << average << endl;
+
     return 0;
 }
